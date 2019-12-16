bce9ad39dede5518f4d66e245a0c484064862dbc
xiaoming <sc15882450625@sina.com>
2019-12-16 22:10:04 +0800

    Signed-off-by: xiaoming <sc15882450625@sina.com>

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+      <div class="one"></div>
+      <div class="two"></div>
+      <div class="three"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .hello{
+    width: 800px;
+    height: 500px;
+    border: 1px solid red;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    justify-content: space-around;
+    div{
+      width: 200px;
+      height: 200px;
+      background: red;
+    }
+    .one{
+
+    }
+  }
+</style>
